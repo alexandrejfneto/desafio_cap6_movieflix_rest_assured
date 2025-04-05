1a678023e69fd68c26125285c8935bf935f37efb
Alexandre Ferreira <alexandrejfneto@gmail.com>
2025-04-04 21:47:11 -0300

    Movie Tests Implemeted (Partial)

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,24 +1,100 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.*;
+
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dsmovie.tests.TokenUtil;
+
+import io.restassured.http.ContentType;
 
 public class MovieControllerRA {
 	
+	private Long existingMovieId, nonExistingMovieId;
+	
+	private String adminUsername, adminPassword, clientUsername, clientPassword;
+	private String adminToken, clientToken, invalidToken;
+	
+	@BeforeEach
+	public void setup() throws Exception {
+		
+		baseURI = "http://localhost:8080";
+		
+		existingMovieId = 1L;
+		nonExistingMovieId = 100L;
+		
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		
+		
+	}
+	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		
+		given()
+	.when()
+		.get("/movies")
+	.then()
+		.statusCode(200)
+		.body("content.title", hasItems("The Witcher", "Venom: Tempo de Carnificina"))
+		;
+		
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		
+		String title = "Venom";
+		
+		given()
+		.when()
+			.get("/movies?title={title}", title)
+		.then()
+			.statusCode(200)
+			.body("content.title", hasItems("Venom: Tempo de Carnificina"))
+			;
+		
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {	
+
+		given()
+		.when()
+			.get("/movies/{id}", existingMovieId)
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("title", equalTo("The Witcher"))
+			.body("score", equalTo(4.33F))
+			.body("count", is(3))
+			.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"))
+			;
+		
 	}
 	
 	@Test
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+		
+		given()
+		.when()
+			.get("/movies/{id}", nonExistingMovieId)
+		.then()
+			.statusCode(404)
+			.body("status", is(404))
+			.body("error", equalTo("Recurso não encontrado"))
+			;
+		
 	}
 	
 	@Test
